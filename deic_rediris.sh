a1221adec17645f54b4408e1b646dd73f76725e1
joan667 <44604896+joan667@users.noreply.github.com>
2025-03-06 14:34:14 +0100

    Update deic_rediris.sh

--- deic_rediris.sh
+++ deic_rediris.sh
@@ -1,3 +1,4 @@
-sudo iptables -A OUTPUT -d 158.109.79.0/24 -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --dport 21 -d ftp.rediris.es -j ACCEPT
-sudo iptables -A OUTPUT -j DROP
+#!/bin/bash
+sudo iptables -A FORWARD -d 158.109.79.0/24 -s 192.168.2.0/24 -j ACCEPT
+sudo iptables -A FORWARD -p tcp --dport 21 -d ftp.rediris.es -s 192.168.2.0/24 -j ACCEPT
+sudo iptables -A FORWARD -s 192.168.2.0/24 -j DROP
