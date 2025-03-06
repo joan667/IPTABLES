327edc0f10f68e616ddd9823fcbcec7f7c6c4fdc
joan667 <44604896+joan667@users.noreply.github.com>
2025-03-06 14:36:45 +0100

    Update gateway.sh

--- gateway.sh
+++ gateway.sh
@@ -8,13 +8,9 @@ iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 /sbin/sysctl -w net.ipv4.conf.eth1.forwarding=1
 sysctl -p
 
-#Opcional para evitar conexiones entrantes de usuarios no autorizados a la propia gateway
-#iptables -A INPUT -m tos --tos 0x10 -j DROP
-#iptables -A INPUT -m tos --tos 0x20 -j DROP
-
-iptables -t mangle -A FORWARD -m tos --tos 0x10 -j DROP
-iptables -t mangle -A FORWARD -m tos --tos 0x20 -j DROP
+iptables -t mangle -A PREROUTING -m tos --tos 0x10 -j DROP
+iptables -t mangle -A PREROUTING -m tos --tos 0x20 -j DROP
 
 #a revisar
-iptables -t mangle -A FORWARD -p tcp -m tos --tos 0x20 -j LOG --log-prefix "IPTABLES - USER2" --log-level info
-iptables -t mangle -A FORWARD -p tcp -m tos --tos 0x10 -j LOG --log-prefix "IPTABLES - USER1" --log-level info
+iptables -t mangle -A PREROUTING -p tcp -m tos --tos 0x20 -j LOG --log-prefix "[IPTABLES - USER2]" --log-level info
+iptables -t mangle -A PREROUTING -p tcp -m tos --tos 0x10 -j LOG --log-prefix "[IPTABLES - USER1]" --log-level info
